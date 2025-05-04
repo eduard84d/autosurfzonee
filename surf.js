da9335120d5dc0e000e69f30e6affc3e3796c035
eduard84d <99147522+eduard84d@users.noreply.github.com>
2025-05-04 13:13:36 +0200

    Update surf.js

--- surf.js
+++ surf.js
@@ -1,9 +1,14 @@
 
 const ads = [
   "https://antautosurf.com/?ref=29085",
-  "https://coinpayu.com/?r=eduard85",
   "https://faucetpay.io/?r=2738792",
   "https://rollercoin.com/?r=kndi047d"
+  "https://socpublic.com/?i=9480820&slide=1",
+  "https://bitsbon.com/?ref=5148",
+  "https://payeer.com/?session=12482302",
+  "https://viefaucet.com?r=61e83d735cc8356fa4bded97",
+  "https://www.binance.com/referral/earn-together/refertoearn2000usdc/claim?hl=ru&ref=GRO_14352_KKBV3&utm_medium=web_share_copy&utm_source=referralmode",
+  
 ];
 
 let index = 0;
